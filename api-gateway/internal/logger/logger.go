a101a07e2f0df95a74a2aa75bef0ad55792f4fb0
TasneemEltabakh <s-tasneem.attia@zewailcity.edu.eg>
2025-04-18 21:09:13 +0200

    chore: change in parameter name

--- api-gateway/internal/logger/logger.go
+++ api-gateway/internal/logger/logger.go
@@ -6,13 +6,13 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-var log = logrus.New()
+var Log = logrus.New()
 
 func init() {
-	log.SetOutput(os.Stdout)
-	log.SetFormatter(&logrus.JSONFormatter{
+	Log.SetOutput(os.Stdout)
+	Log.SetFormatter(&logrus.JSONFormatter{
 		PrettyPrint:     true,
 		TimestampFormat: "2006-01-02T15:04:05Z07:00",
 	})
-	log.SetLevel(logrus.InfoLevel)
+	Log.SetLevel(logrus.InfoLevel)
 }
