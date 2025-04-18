9f121009199a2b5691abad75c2a2828b306b1dc6
TasneemEltabakh <s-tasneem.attia@zewailcity.edu.eg>
2025-04-18 21:33:43 +0200

    feat: create main.go

--- api-gateway/cmd/gateway/main.go
+++ api-gateway/cmd/gateway/main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/besology512/api-gateway/internal/config"
+	"github.com/besology512/api-gateway/internal/gateway"
+	"github.com/besology512/api-gateway/internal/logger"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	config := config.Load()
+	router := gateway.APIGateWayServer(config)
+	addr := fmt.Sprintf(":%s", config.Port)
+	logger.Log.Infof("starting API Gateway on %s", addr)
+	if err := router.Run(addr); err != nil {
+		log.Fatalf("failed to start gateway: %v", err)
+
+	}
 }
